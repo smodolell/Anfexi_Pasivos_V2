7e7ac5854667387204a866c16d3bff076b520fe6
Sergio Modolell <sergio.modolell@gmail.com>
2026-04-08 16:18:49 -0300

    Nueva variable de entorno

---
+++ src/Anfx.Pasivos.Frontend/src/environments/environment.production.anfexi.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  apiBaseUrl:      'https://dev.anfexi.com/profuturo/pasivos/backend',
+  apiAuthUrl:      'https://dev.anfexi.com/profuturo/pasivos/backend/api',
+  apiCatalogoUrl:  'https://dev.anfexi.com/profuturo/pasivos/backend/api',
+  apiSistemaUrl:   'https://dev.anfexi.com/profuturo/pasivos/backend/api',
+  apiCotizadorUrl: 'https://dev.anfexi.com/profuturo/pasivos/backend/api',
+  menuApiUrl: 'assets/menu.json',
+  // ── App metadata ────────────────────────────────────────────
+  appName:    'Pasivos',
+  appVersion: '0.0.1',         // sincronizar con package.json al hacer release
+  company:    'ANFEXI TECHNOLOGIES',
+};
